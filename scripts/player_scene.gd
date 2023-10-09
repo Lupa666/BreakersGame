b72dc04a9faa814d79007da1b1a3933d9d7066d6
BSojka <bartkowski_so@interia.pl>
2023-10-10 01:13:32 +0200

    Added smooth movement

--- scripts/player_scene.gd
+++ scripts/player_scene.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
-@export var SPEED: float = 300.0
+@export var MAX_SPEED: float = 600.0
+@export var ACCELERATION: float = 5000
+@export var DECELERATION: float = 5000
+
+@onready var PLAYER_SPRITE = $PlayerSprite2D
 
 func _process(delta: float):
+	print(velocity)
+	
+	if velocity.x > 0:
+		PLAYER_SPRITE.flip_h = false
+	else:
+		PLAYER_SPRITE.flip_h = true
 	return
 
 func _physics_process(delta: float):
@@ -11,12 +21,17 @@ func _physics_process(delta: float):
 	
 
 func handle_movement(delta: float) -> KinematicCollision2D:
-	var velocity: Vector2 = get_direction()
+	var direction: Vector2 = get_direction()
+	
+	if direction:
+		velocity = velocity.move_toward(direction*MAX_SPEED, ACCELERATION*delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
+	
 	return move_and_collide(velocity * delta)
 
 func get_direction() -> Vector2:
 	var direction: Vector2 = Vector2.ZERO
 	direction.x = Input.get_axis("play_left", "play_right")
 	direction.y = Input.get_axis("play_up", "play_down")
-	print(direction.normalized() * SPEED)
-	return direction.normalized() * SPEED
+	return direction.normalized()
