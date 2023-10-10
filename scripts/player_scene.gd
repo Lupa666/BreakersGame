2b083f7f1971e925900fe6eb44a937fdb6682a28
BSojka <bartkowski_so@interia.pl>
2023-10-10 20:38:58 +0200

    Added new sprite (better for testing the project) and basic animations with animation tree

--- scripts/player_scene.gd
+++ scripts/player_scene.gd
@@ -1,37 +1,44 @@
 extends CharacterBody2D
 
 @export var MAX_SPEED: float = 600.0
-@export var ACCELERATION: float = 5000
-@export var DECELERATION: float = 5000
+@export var ACCELERATION: float = 6000.0
+@export var DECELERATION: float = 6000.0
 
-@onready var PLAYER_SPRITE = $PlayerSprite2D
+@onready var PLAYER_ANIMATION_TREE: AnimationTree = $Character/CharacterAnimationTree
+@onready var PLAYER_ANIMATION_STATE: AnimationNodeStateMachinePlayback = PLAYER_ANIMATION_TREE.get("parameters/playback")
+
+#enum DIRECTION {UP, DOWN, LEFT, RIGHT}
+
+func _ready():
+	pass
 
 func _process(delta: float):
-	print(velocity)
-	
-	if velocity.x > 0:
-		PLAYER_SPRITE.flip_h = false
-	else:
-		PLAYER_SPRITE.flip_h = true
-	return
+	pass
 
 func _physics_process(delta: float):
 	# Get the input direction and handle the movement/deceleration.
 	handle_movement(delta)
 	
 
-func handle_movement(delta: float) -> KinematicCollision2D:
+func handle_movement(delta: float) -> bool:
 	var direction: Vector2 = get_direction()
 	
 	if direction:
+		PLAYER_ANIMATION_TREE.set("parameters/idle/blend_position", direction)
+		PLAYER_ANIMATION_TREE.set("parameters/run/blend_position", direction)
+		PLAYER_ANIMATION_STATE.travel("run")
 		velocity = velocity.move_toward(direction*MAX_SPEED, ACCELERATION*delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
+		PLAYER_ANIMATION_STATE.travel("idle")
 	
-	return move_and_collide(velocity * delta)
+	return move_and_slide()
 
 func get_direction() -> Vector2:
 	var direction: Vector2 = Vector2.ZERO
 	direction.x = Input.get_axis("play_left", "play_right")
 	direction.y = Input.get_axis("play_up", "play_down")
 	return direction.normalized()
+
+
+
